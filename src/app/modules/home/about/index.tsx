f9fd33d03a02e658863a2c3542adab0d392c9b02
Aftar Fadilah <aftar.fadilah@nosc.ai>
2023-09-23 21:42:05 +0700

    Add about agro asri content

--- src/app/modules/home/about/index.tsx
+++ src/app/modules/home/about/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
   toptext?: string;
@@ -12,10 +13,16 @@ type Props = {
     url: string;
     alt: string;
   }[];
-  cards?: string[]
+  cards?: string[];
 };
 
-const About: React.FC<Props> = ({ toptext, title, deskripsi, medias }) => {
+const About: React.FC<Props> = ({
+  toptext,
+  title,
+  deskripsi,
+  medias,
+  cards,
+}) => {
   return (
     <div className="w-full text-gray-900">
       <div className="max-w-5xl mx-auto flex flex-col sm:flex-row gap-x-16 px-4 sm:px-0 py-24 gap-12">
@@ -69,19 +76,21 @@ const About: React.FC<Props> = ({ toptext, title, deskripsi, medias }) => {
           <h2 className="text-6xl font-black">{title}</h2>
           <p>{deskripsi}</p>
           <div className="w-full flex justify-end">
-            <span>Info lebih lanjut</span>
+            <Link href="/agro-asri">Info lebih lanjut</Link>
           </div>
-          <div className="flex flex-col gap-y-4">
-            <div className="flex flex-row gap-x-4">
-              <div className="bg-gray-200 rounded-md w-full flex flex-col sm:flex-row">
-                <div className="flex justify-center items-center p-2 px-4">
-                  <div className="bg-white rounded-full aspect-square w-[50px]"></div>
-                </div>
-                <div className="py-4 px-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          <div className="grid grid-cols-2 gap-4">
+            {cards?.map((card, index) => (
+              <div className="flex flex-row gap-x-4" key={index}>
+                <div className="bg-gray-200 rounded-md w-full flex flex-col sm:flex-row">
+                  <div className="flex justify-center items-center p-2 px-4">
+                    <div className="bg-white rounded-full aspect-square w-[50px]"></div>
+                  </div>
+                  <div className="py-4 px-4">
+                    {card}
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
