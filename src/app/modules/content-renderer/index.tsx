3b0c06fa9f1d603b743ba67ffa01f2eb87515df4
Aftar Fadilah <aftar.fadilah@nosc.ai>
2023-09-23 22:53:49 +0700

    Add dynamic content data

---
+++ src/app/modules/content-renderer/index.tsx
@@ -0,0 +1,61 @@
+import clientConfig from "../../../../client-config";
+import HeroBanner from "../hero-banner";
+
+type Props = {
+  data: any;
+  type: "ComponentTampilanBanner" | "";
+};
+
+const ContentRenderer: React.FC<Props> = ({ type, data }) => {
+  if (type === "ComponentTampilanBanner") {
+    return (
+      <ComponentTampilanBanner
+        background={{
+          alt: data.latar_belakang.data.attributes.alternativeText,
+          url: `${clientConfig.strapiUrl}${data.latar_belakang.data.attributes.url}`,
+        }}
+        button={{ text: data.link_teks, url: data.link_url }}
+        description={data.deskripsi}
+        title={data.judul}
+      />
+    );
+  }
+  return <></>;
+};
+
+type TampilanBannerProp = {
+  title?: string;
+  description?: string;
+  button?: {
+    text: string;
+    url: string;
+  };
+  background?: {
+    url: string;
+    alt: string;
+  };
+};
+
+const ComponentTampilanBanner: React.FC<TampilanBannerProp> = ({
+  title,
+  description,
+  button,
+  background,
+}) => {
+  return (
+    <HeroBanner
+      title={title}
+      description={description}
+      link={{
+        text: button.text,
+        url: button.url,
+      }}
+      background={{
+        url: background.url,
+        alt: background.alt,
+      }}
+    />
+  );
+};
+
+export default ContentRenderer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import OurProducts from "./modules/home/our-products";
 import HeroBanner from "./modules/hero-banner";
 import { gql, useQuery } from "@apollo/client";
 import clientConfig from "../../client-config";
+import ContentRenderer from "./modules/content-renderer";
 
 export default function Home() {
   const [data, setData] = useState<any>(null);
@@ -16,7 +17,7 @@ export default function Home() {
     error,
     data: dataQuery,
   } = useQuery(gql`
-    query QueryHome {
+    query HomeQuery {
       home {
         data {
           attributes {
@@ -27,6 +28,7 @@ export default function Home() {
                 attributes {
                   url
                   alternativeText
+                  provider
                 }
               }
             }
@@ -64,6 +66,14 @@ export default function Home() {
                 deskripsi
                 link_teks
                 link_url
+                latar_belakang {
+                  data {
+                    attributes {
+                      url
+                      alternativeText
+                    }
+                  }
+                }
               }
             }
             createdAt
@@ -75,23 +85,24 @@ export default function Home() {
     }
   `);
 
-  console.log(">> dq", dataQuery)
-
   if (error) {
     return <></>;
   }
 
   // Hero Const
   const homeContent = dataQuery?.home?.data?.attributes;
-  const heroBanner = homeContent?.banner.data[0].attributes
+  const heroBanner = homeContent?.banner.data[0].attributes;
 
   // About Const
   const aboutMedias = homeContent?.gallery.data.map((media) => {
-    return {url: `${clientConfig.strapiUrl}${media.attributes.url}`, alt: media.attributes.alternativeText}
-  })
+    return {
+      url: `${clientConfig.strapiUrl}${media.attributes.url}`,
+      alt: media.attributes.alternativeText,
+    };
+  });
   const aboutCards = homeContent?.small_card.map((card) => {
-    return card.isi
-  })
+    return card.isi;
+  });
 
   // Feat. Products
   const products = homeContent?.kotak_produk.map((produk) => {
@@ -100,9 +111,9 @@ export default function Home() {
       image: {
         url: `${clientConfig.strapiUrl}${produk.foto_produk.data.attributes.url}`,
         alt: produk.foto_produk.data.attributes.alternativeText,
-      }
-    }
-  })
+      },
+    };
+  });
 
   return (
     <main className="flex flex-col items-center justify-between">
@@ -119,30 +130,9 @@ export default function Home() {
         cards={aboutCards}
       />
       <OurProducts products={products} />
-      <HeroBanner
-        title="Produk Terbaru"
-        description="Hadirlah Inovasi Baru Sebuah ICE CREAM  Labu Dari UMKM Sekitar"
-        link={{
-          text: "Aku Juga Mw",
-          url: "/es-krim-labu-madu",
-        }}
-        background={{
-          url: "https://images.unsplash.com/photo-1595871277397-08901ed2d7f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-          alt: "Ice Cream Banner",
-        }}
-      />
-      <HeroBanner
-        title="Pupuk Alami"
-        description="Menggunakan racikan alami demi kesehatan yang maksimal"
-        link={{
-          text: "Aku Juga Mw",
-          url: "/pupuk",
-        }}
-        background={{
-          url: "https://images.unsplash.com/photo-1416879595882-3373a0480b5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-          alt: "Pupuk Banner",
-        }}
-      />
+      {homeContent?.konten.map((content: any, index: number) => (
+        <ContentRenderer data={content} type={content.__typename} key={index} />
+      ))}
     </main>
   );
 }
